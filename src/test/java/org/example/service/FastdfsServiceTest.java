6899151a162609ce493d4d392c9a68f8a175d79f
hanbing <hanbing@wanji.net.cn>
2023-12-06 10:07:46 +0800

    不通过Util，以单元测试的方式执行，失败。错误码：2，错误信息：找不到节点或文件

--- src/test/java/org/example/service/FastdfsServiceTest.java
+++ src/test/java/org/example/service/FastdfsServiceTest.java
@@ -1,11 +1,19 @@
 package org.example.service;
 
+import com.github.tobato.fastdfs.domain.fdfs.MetaData;
+import com.github.tobato.fastdfs.domain.fdfs.StorePath;
+import com.github.tobato.fastdfs.service.FastFileStorageClient;
+import org.apache.commons.io.FilenameUtils;
 import org.example.MainTest;
 import org.example.util.FastDfsUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.nio.file.Files;
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,15 +21,24 @@ import static org.junit.jupiter.api.Assertions.*;
 class FastdfsServiceTest extends MainTest {
 
     @Autowired
-    private FastDfsUtils fastDfsUtils;
+    protected FastFileStorageClient storageClient;
 
     @Test
     void fastdfsTest() throws Exception {
-        String serverUrl = fastDfsUtils.serverUrl;
-        System.out.println(serverUrl);
 
         File file = new File("afile.data");
-        String s = fastDfsUtils.writeInputStreamToFile(file);
-        System.out.println(s);
+        Set<MetaData> metaDataSet = createMetaData();
+
+        StorePath path = storageClient.uploadFile(Files.newInputStream(file.toPath()), file.length(),
+                FilenameUtils.getExtension(file.getName()), metaDataSet);
+
+        System.out.println(path);
+    }
+
+    private Set<MetaData> createMetaData() {
+        Set<MetaData> metaDataSet = new HashSet<>();
+        metaDataSet.add(new MetaData("Author", "tobato"));
+        metaDataSet.add(new MetaData("CreateDate", "2016-01-05"));
+        return metaDataSet;
     }
 }
